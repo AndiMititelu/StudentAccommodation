682ea257dada302be5ebe81775a77542bb9ad8f3
AndiMititelu <41987168+AndiMititelu@users.noreply.github.com>
2023-08-20 18:22:09 +0300

    Delete studentAccommodation/src/main/resources/static directory

--- studentAccommodation/src/main/resources/static/animation.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const loginLink = document.getElementById("login-link");
-  loginLink.addEventListener("click", function() {
-    // Generate the /login link and navigate to it
-    const loginUrl = "/login";
-    window.location.href = loginUrl;
-  });
-});
